4ac669eb483644d879b39023005164c91cf4fc1f
SufyanButt <muhammadsufyan60738@gmail.com>
2025-04-15 22:53:58 +0500

    new commigjhgjh

--- lib/ip_address.dart
+++ lib/ip_address.dart
@@ -1,5 +1,5 @@
 class NetworkConfig{
- String _ipAddress = "10.210.246.44";  // Private variable
+ String _ipAddress = "192.168.100.179";  // Private variable
   //String _ipAddress = "192.168.1.29";  // Private variable
   String get ipAddress => _ipAddress;  // Getter method
 
